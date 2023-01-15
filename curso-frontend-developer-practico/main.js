680f7e12fcfe4de815d4d4a04237a924602a78a5
German Bartoli <bartoligerman497@gmail.com>
2023-01-15 18:56:26 -0300

    Prettier

--- curso-frontend-developer-practico/main.js
+++ curso-frontend-developer-practico/main.js
@@ -56,7 +56,7 @@ const productList = [];
 productList.push({
     name: "Bike",
     price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 
 /* 
@@ -80,42 +80,42 @@ productList.push({
 </div>; 
 */
 
-function renderProducts(arr){
-for (product of productList) {
-    const productCard = document.createElement("div")
-    productCard.classList.add("product-card");
+function renderProducts(arr) {
+    for (product of productList) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
 
-    const productImg = document.createElement("img")
-    // product = {name, price, image} -> product.image
-    productImg.setAttribute("src", product.image);
+        const productImg = document.createElement("img");
+        // product = {name, price, image} -> product.image
+        productImg.setAttribute("src", product.image);
 
-    const productInfo = document.createElement("div")
-    productInfo.classList.add("product-info");
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
 
-    const productInfoDiv = document.createElement("div")
+        const productInfoDiv = document.createElement("div");
 
-    const productPrice = document.createElement("p")
-    productPrice.innerText = "$" + product.price;
-    const productName = document.createElement("p")
-    productName.innerText = product.name;
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + product.price;
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
 
-    productInfoDiv.appendChild(productPrice)
-    productInfoDiv.appendChild(productName)
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
 
-    const productInfoFigure = document.createElement("figure")
-    const productImgCart = document.createElement("img")
-    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
 
-    productInfoFigure.appendChild(productImgCart)
+        productInfoFigure.appendChild(productImgCart);
 
-    productInfo.appendChild(productInfoDiv)
-    productInfo.appendChild(productInfoFigure)
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
 
-    productCard.appendChild(productImg)
-    productCard.appendChild(productInfo)
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
 
-    cardsContainer.appendChild(productCard)
+        cardsContainer.appendChild(productCard);
+    }
 }
-}
 
-renderProducts(productList)
+renderProducts(productList);
