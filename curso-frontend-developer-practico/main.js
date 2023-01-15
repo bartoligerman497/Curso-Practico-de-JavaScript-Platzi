5309309c956a24f00f612713250253457ea192f9
German Bartoli <bartoligerman497@gmail.com>
2023-01-15 13:03:48 -0300

    Prettier

--- curso-frontend-developer-practico/main.js
+++ curso-frontend-developer-practico/main.js
@@ -1,18 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive")
+    desktopMenu.classList.toggle("inactive");
 }
 
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-menuHamIcon.addEventListener("click", toggleMobileMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive")
+    mobileMenu.classList.toggle("inactive");
 }
-
