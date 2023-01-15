b5b4a165b2b118c5526c4b8ea64f44b4097b4329
German Bartoli <bartoligerman497@gmail.com>
2023-01-15 13:03:21 -0300

    Fusión del Menú en Mobile: Clase 19

--- curso-frontend-developer-practico/main.js
+++ curso-frontend-developer-practico/main.js
@@ -1,9 +1,18 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-
 menuEmail.addEventListener("click", toggleDesktopMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive")
 }
+
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuHamIcon.addEventListener("click", toggleMobileMenu)
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
+}
+
